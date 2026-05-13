142fe13440a4e9316d70094c69e7466c519d71ed
Rico-Jetvendra <rico.jetvendra@gmail.com>
2026-05-13 15:50:20 +0700

    Rollback

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider{
     /**
@@ -30,7 +29,5 @@ class AppServiceProvider extends ServiceProvider{
             );
 
         });
-
-        URL::forceRootUrl(config('app.url'));
     }
 }
